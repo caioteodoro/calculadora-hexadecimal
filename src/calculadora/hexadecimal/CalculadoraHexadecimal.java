63280b1ed51794e1c8462a9a6133cb530b63a51e
caioteodoro <caiorodrigot@hotmail.com>
2022-03-21 21:12:17 -0300

    project concluded

--- src/calculadora/hexadecimal/CalculadoraHexadecimal.java
+++ src/calculadora/hexadecimal/CalculadoraHexadecimal.java
@@ -3,18 +3,40 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package calculadora.hexadecimal;
+import java.util.*;
 
-/**
- *
- * @author teodoro
- */
 public class CalculadoraHexadecimal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int a = sc.nextInt();
+        
+        int numero = a;
+        Stack<String> convertido = new Stack<>();  
+                
+        while (numero != 0) {
+            var resto = numero % 16;
+            if (resto > 9){
+                switch (resto) {
+                    case 10 -> convertido.push("A");
+                    case 11 -> convertido.push("B");
+                    case 12 -> convertido.push("C");
+                    case 13 -> convertido.push("D");
+                    case 14 -> convertido.push("E");
+                    case 15 -> convertido.push("F");
+                }
+            } else {
+                convertido.push(String.format("%d", resto));
+            }
+            numero = numero / 16;
+        }
+        
+        while (!convertido.empty()) {
+            var topo = convertido.peek();
+            System.out.print(topo);
+            convertido.pop();
+        }
+        System.out.print("\n");
     }
-    
 }
